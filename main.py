0a3d53e46528c8f0715a4ae1be98958990138643
Calvin Zhang <54903512+zzhang-1@users.noreply.github.com>
2023-11-01 20:26:39 -0400

    Update main.py
    latest code for this file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import cv2 as cv
 import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models, applications
 
-data_dir = "/Users/zhenzhang/Desktop/CS courses /NGHackWeekTeam4/TRAIN_txt"
+data_dir = "/Users/zhenzhang/Desktop/CS courses /NGHackWeekTeam4/TRAIN_txt" # replace the path
 
 ####  we are using the VGG16 model from the tensorflow.keras.applications
 def load_image(image_path, convert):
@@ -31,9 +33,39 @@ def parse_txt(txt_path):
         
         return [label], [box]
 
+def show_images_with_boxes(images, boxes, labels):
+    fig, axes = plt.subplots(4, 5, figsize=(15, 12))  # Adjust the size as needed
+    axes = axes.ravel()
+
+    for i in np.arange(0, 20):  # Show the first 20 images
+        axes[i].imshow(images[i])
+        axes[i].set_title(f'Label: {label_to_index[labels[i]]}', fontsize=12)
+        
+        # Add bounding box if label is 0 (drone)
+        if labels[i] == 0:
+            # Convert bounding box from relative coordinates to image coordinates
+            height, width, _ = images[i].shape
+            center_x, center_y, w, h = boxes[i]
+            x = int((center_x * width) - (w * width) / 2)
+            y = int((center_y * height) - (h * height) / 2)
+            w = int(w * width)
+            h = int(h * height)
+
+            # Create a rectangle patch and add it to the axis
+            rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+            axes[i].add_patch(rect)
+        
+        # Remove axes for cleaner look
+        axes[i].axis('off')
+
+    plt.subplots_adjust(wspace=0.5)
+    plt.show()
+
+
 images = []
 all_labels = []
 all_boxes = []
+
 
 label_to_index = {0: "drone", 1: "no drone"}  # Define mapping for labels
 index_to_label = {"drone": 0, "no drone": 1} # useful when we test our model on new images
@@ -57,6 +89,10 @@ images = np.array(images)
 all_labels = np.array(all_labels)
 all_boxes = np.array(all_boxes)
 
+
+# Call the function to display images
+show_images_with_boxes(images, all_boxes, all_labels)
+# build the model 
 base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 for layer in base_model.layers:
     layer.trainable = False
@@ -81,7 +117,9 @@ X_train, X_val, y_train_boxes, y_val_boxes, y_train_labels, y_val_labels = train
 
 model.fit(X_train, {'bbox_output': y_train_boxes, 'class_output': y_train_labels},
           validation_data=(X_val, {'bbox_output': y_val_boxes, 'class_output': y_val_labels}),
-          epochs=1, batch_size=32)
+          epochs=10, batch_size=32)
+
+
 
 test_image = load_image("/Users/zhenzhang/Desktop/CS courses /NGHackWeekTeam4/testimage/test.jpg", True)
 test_res = model.predict(test_image)
@@ -92,16 +130,14 @@ class_name = label_to_index[class_label_index]
 
 print(f'The predicted class is: {class_name}')
 
-test_image1 = load_image("/Users/zhenzhang/Desktop/CS courses /NGHackWeekTeam4/testimage/test.jpg", True)
+test_image1 = load_image("/Users/zhenzhang/Desktop/CS courses /NGHackWeekTeam4/testimage/test3.jpg", True)
 test_res1 = model.predict(test_image1)
 # Get the class label index with the highest probability
 class_label_index1 = np.argmax(test_res1[1])
 # Map the index to the class name
 class_name1 = label_to_index[class_label_index1]
 print(f'The predicted class is: {class_name1}')
-"""
-model.save("path_to_save_model/my_model") # save the model and so that we can use for later pull out easier
-from tensorflow.keras.models import load_model # import the model 
-loaded_model = load_model("path_to_save_model/my_model") # use on test new data
-predictions = loaded_model.predict(some_input_data) # do the prediction for new data
-"""
+
+model.save("drone_detection_model/my_model")  # save the model
+
+
